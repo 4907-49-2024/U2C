d5af0f62a624e6cea83352299b31b11aaef9bcbf
orselane <77144245+orselane@users.noreply.github.com>
2025-01-30 11:21:26 -0500

    StateDiagram pipe type
    - Included methods to get fields
    - Included method to register elements post-construction (likely to be built progressively).

---
+++ src/main/java/pipes/diagrams/StateDiagram.java
@@ -0,0 +1,73 @@
+package pipes.diagrams;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * State diagram internal representation.
+ * Contains its states, transitions and a name to map it to an agent.
+ */
+public class StateDiagram {
+    private final String name;
+    private final Set<State> states;
+    private final Set<Transition> transitions;
+    private State initialState;
+
+    public StateDiagram(String name) {
+        this.name = name;
+        this.states = new HashSet<>();
+        this.transitions = new HashSet<>();
+    }
+
+    /**
+     * Registers a state diagram element to its collection
+     * <br>
+     * TODO: Superstates are going to be a bit complicated. Taking some notes here temporarily to avoid forgetting.
+     *   Make sure to check these behaviours when we implement it outside:
+     *  - Child states inherit the name of their parent, but have different activities
+     *  - Parent has no activity itself, and thus should never be a state itself (Only state which does this!)
+     *  - Transitions go to both child states (should be easy enough)
+     *  - Single Transition out gets duplicated to be sourced from both child states.
+     *
+     * @param element The element to register
+     */
+    public void registerElement(StateDiagramElement element) {
+        if (element instanceof State s) {
+            if (s.kind().isEmpty()) {
+                if (initialState == null) {
+                    initialState = s;
+                } else {
+                    // This may technically be legal in concurrent regions, which we don't handle yet.
+                    throw new IllegalStateException("There exists more than one initial State.");
+                }
+            }
+
+            this.states.add(s);
+        } else if (element instanceof Transition t) {
+            this.transitions.add(t);
+        } else {
+            throw new IllegalArgumentException("Element is not a known StateDiagramElement");
+        }
+    }
+
+    /**
+     * @return Diagram name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return Diagram States
+     */
+    public Set<State> getStates() {
+        return states;
+    }
+
+    /**
+     * @return Diagram Transitions
+     */
+    public Set<Transition> getTransitions() {
+        return transitions;
+    }
+}
