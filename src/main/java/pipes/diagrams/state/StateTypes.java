8941d7f7e3bf9c1277e7eaf759a2da7280b96937
orselane <77144245+orselane@users.noreply.github.com>
2025-02-03 18:10:24 -0500

    Defined StateTypes enum (based on metamodel)

---
+++ src/main/java/pipes/diagrams/state/StateTypes.java
@@ -0,0 +1,12 @@
+package pipes.diagrams.state;
+
+/**
+ * The relevant MetaModel data types for state diagram models.
+ * May be somewhat dependent on the modelling tool, based on Papyrus.
+ */
+public enum StateTypes {
+    statemachine,
+    state,
+    transition,
+    activity
+}
