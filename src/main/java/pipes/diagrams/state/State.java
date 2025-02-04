2c8f3da7395459432c797105700c431fd5b16a6b
orselane <77144245+orselane@users.noreply.github.com>
2025-02-04 10:04:17 -0500

    Adding a parent field to State
    - Makes it easier to track superstate owner, and who is a child state

--- src/main/java/pipes/diagrams/state/State.java
+++ src/main/java/pipes/diagrams/state/State.java
@@ -6,14 +6,16 @@ package pipes.diagrams.state;
  * @param name The name of the state - Note: not unique (children of guarded clause superstates inherit the name!)
  * @param kind The kind of the pseudo-state, N/A if not a pseudo-state (For Papyrus, Empty = Initial)
  * @param doActivity The doActivity, if set. Null otherwise.
+ * @param parent The superstate containing this state, if applicable. Note: if set, name should be null
  */
-public record State(String name, String kind, String doActivity) implements StateDiagramElement {
+public record State(String name, String kind, String doActivity, State parent) implements StateDiagramElement {
     /**
      * Converts null objects to empty strings, otherwise stores as is.
      */
-    public State(String name, String kind, String doActivity){
+    public State(String name, String kind, String doActivity, State parent) {
         this.name = name == null ? "" : name;
         this.kind = kind == null ? "" : kind;
         this.doActivity = doActivity == null ? "" : doActivity;
+        this.parent = parent;
     }
 }
