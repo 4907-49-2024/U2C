85b10992320f567db9d37cddd7c7be8e17923e0b
orselane <77144245+orselane@users.noreply.github.com>
2025-02-03 19:15:16 -0500

    Adding XMIParserTest for state diagrams.

--- src/test/java/filterTests/xmiParser/XMIParserTest.java
+++ src/test/java/filterTests/xmiParser/XMIParserTest.java
@@ -1,14 +1,58 @@
 package filterTests.xmiParser;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import com.sdmetrics.model.ModelElement;
+import filters.xmiParser.XMIParser;
+import filters.xmiParser.XMIParserConfig;
+import org.junit.jupiter.api.Test;
+import pipes.UMLModel;
+import pipes.diagrams.state.StateTypes;
+
+import java.util.List;
 
 /**
  * Test that all diagrams have the elements we expect.
- *
  */
 class XMIParserTest {
-    testStateElements(){
-        // Setup XMI Parse
 
+    /**
+     * Test for state diagram elements
+     */
+    @Test
+    void testStateElements() throws Exception {
+        // Setup XMI Parse config - state based
+        String xmiFile = "superState.uml";
+        String metaModel = "custom/stateMetaModel.xml";
+        String xmiTrans = "custom/xmiStateTrans.xml";
+        XMIParser parser = new XMIParser(new XMIParserConfig(xmiFile, xmiTrans, metaModel));
+
+        // Collect output
+        UMLModel model = parser.getModel();
+        List<ModelElement> states = model.getTypedElements(StateTypes.state.name());
+        List<ModelElement> transitions = model.getTypedElements(StateTypes.transition.name());
+        List<ModelElement> activity = model.getTypedElements(StateTypes.activity.name());
+        List<ModelElement> statemachines = model.getTypedElements(StateTypes.statemachine.name());
+
+        // Test States
+        assert states.size() == 5; // Includes pseudo-states
+        assert states.stream().anyMatch(s -> s.getName().equals("State1"));
+        assert states.stream().anyMatch(s -> s.getName().equals("State4"));
+        assert states.stream().anyMatch(s -> s.getName().equals("Inner1"));
+        assert states.stream().anyMatch(s -> s.getName().equals("Inner2"));
+        assert states.stream().anyMatch(s -> s.getName().isEmpty());
+
+        // Test Transitions
+        assert transitions.size() == 4; // Includes pseudo-states
+        assert transitions.stream().anyMatch(s -> s.getName().equals("nextState"));
+        assert transitions.stream().anyMatch(s -> s.getName().isEmpty());
+
+        // Test StateDiagrams
+        assert statemachines.size() == 1; // Includes pseudo-states
+        assert statemachines.stream().anyMatch(s -> s.getName().equals("StateMachine1"));
+
+        // Test StateDiagrams
+        assert activity.size() == 3; // Includes pseudo-states
+        assert activity.stream().anyMatch(s -> s.getName().equals("sendStim1"));
+        assert activity.stream().anyMatch(s -> s.getName().equals("sendStim2"));
+        assert activity.stream().anyMatch(s -> s.getName().equals("sendStim3"));
     }
 }
