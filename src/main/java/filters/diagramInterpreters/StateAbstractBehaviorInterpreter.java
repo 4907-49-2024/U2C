b1a21594bf15d3d8a4e46778aa4a9b6912151705
orselane <77144245+orselane@users.noreply.github.com>
2025-03-11 17:44:13 -0400

    Declared new filter scope
    - Missing output pipe representation

---
+++ src/main/java/filters/diagramInterpreters/StateAbstractBehaviorInterpreter.java
@@ -0,0 +1,19 @@
+package filters.diagramInterpreters;
+
+import pipes.UMLModel;
+import pipes.diagrams.state.StateDiagram;
+
+import java.util.Set;
+
+/**
+ * The StateAbstractBehaviorInterpreter takes in a StateDiagram, and returns a representation of its AbstractBehaviors.
+ */
+public class StateAbstractBehaviorInterpreter {
+    // INPUT
+    private final StateDiagram diagram;
+    // TODO: OUTPUT
+
+    public StateAbstractBehaviorInterpreter(StateDiagram diagram) {
+        this.diagram = diagram;
+    }
+}
