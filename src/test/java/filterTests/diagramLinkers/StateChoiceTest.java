a0b82ffcce385c67dd4de69e7db6c1f3f47b6be3
ahmedbabar <ahmedbabar0708@gmail.com>
2025-03-11 09:47:56 -0400

    Added choice atomic assignment and atomic where the last 2 dont work

--- src/test/java/filterTests/diagramLinkers/StateChoiceTest.java
+++ src/test/java/filterTests/diagramLinkers/StateChoiceTest.java
@@ -13,47 +13,6 @@ import java.util.Set;
 
 
 public class StateChoiceTest {
-    @Test
-    public void testStateChoice() throws Exception {
-        // Setup Input
-        String xmiFile = "C2KA-BaseRepresentations/Choice.uml";
-        String metaModel = "custom/stateMetaModel.xml";
-        String xmiTrans = "custom/xmiStateTrans.xml";
-        XMIParser parser = new XMIParser(new XMIParserConfig(xmiFile, xmiTrans, metaModel));
-        UMLModel model = parser.getModel();
-        StateDiagramLinker linker = new StateDiagramLinker(model);
 
-        // Start Thread (run filter)
-        Thread t = new Thread(linker);
-        t.start();
-        t.join();
-
-        // Check output
-        Set<StateDiagram> diagrams = linker.getStateDiagrams();
-        // Assuming single diagram, do not need to match it
-        StateDiagram d = diagrams.iterator().next();
-
-        assert d.getName().equals("Behaviour Choice");
-
-        //Check States
-        Set<State> states = d.getStates();
-        assert states.size() == 3;
-        State initial = new State("a+b", "state", "", null); //a+b
-        assert states.contains(initial);
-        State a = new State("a", "state", "", initial);
-        assert states.contains(a);
-        State b = new State("b", "state", "", initial);
-        assert states.contains(b);
-//        for (State state : states) {
-//            System.out.println(state.name());
-//            System.out.println(state.kind());
-//            System.out.println(state.doActivity());
-//            System.out.println(state.parent());
-//        }
-        //CHeck Transitions
-        Set<Transition> transitions = d.getTransitions();
-        assert transitions.isEmpty();
-
-    }
 
 }
