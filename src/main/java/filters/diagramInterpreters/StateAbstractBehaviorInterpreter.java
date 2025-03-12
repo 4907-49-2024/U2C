edc880f5b901af02b68deda5d8d7c2182785c469
Alexandre Marques <alex.marques@live.ca>
2025-03-12 15:23:40 -0400

    Defined output type in filter

--- src/main/java/filters/diagramInterpreters/StateAbstractBehaviorInterpreter.java
+++ src/main/java/filters/diagramInterpreters/StateAbstractBehaviorInterpreter.java
@@ -1,9 +1,8 @@
 package filters.diagramInterpreters;
 
-import pipes.UMLModel;
+import pipes.c2ka.primitives.Behavior;
 import pipes.diagrams.state.StateDiagram;
 
-import java.util.Set;
 
 /**
  * The StateAbstractBehaviorInterpreter takes in a StateDiagram, and returns a representation of its AbstractBehaviors.
@@ -11,7 +10,8 @@ import java.util.Set;
 public class StateAbstractBehaviorInterpreter {
     // INPUT
     private final StateDiagram diagram;
-    // TODO: OUTPUT
+    // OUTPUT (top level behavior: string representation = Abstract Behavior Specification)
+    private Behavior topBehavior;
 
     public StateAbstractBehaviorInterpreter(StateDiagram diagram) {
         this.diagram = diagram;
