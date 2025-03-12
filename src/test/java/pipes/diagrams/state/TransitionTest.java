2b026f932b4126436152e801ba39cc40bb910336
Alexandre Marques <alex.marques@live.ca>
2025-03-12 18:45:15 -0400

    Updated TransitionTest
    - Missing one test case

--- src/test/java/pipes/diagrams/state/TransitionTest.java
+++ src/test/java/pipes/diagrams/state/TransitionTest.java
@@ -14,7 +14,6 @@ public class TransitionTest {
         Transition initialState = new Transition(null, null, "");
         assert initialState.input().isEmpty();
         assert initialState.output().isEmpty();
-        assert initialState.guard().isEmpty();
     }
 
 
@@ -23,7 +22,6 @@ public class TransitionTest {
         Transition normalState =  new Transition(null, null, "stimIn/stimOut");
         assert normalState.input().equals("stimIn");
         assert normalState.output().equals("stimOut");
-        assert normalState.guard().isEmpty();
     }
 
 
@@ -32,6 +30,7 @@ public class TransitionTest {
         Transition guardedState = new Transition (null, null, "stim1[x=1]/stim2");
         assert guardedState.input().equals("stim1");
         assert guardedState.output().equals("stim2");
-        assert guardedState.guard().equals("x=1");
     }
+
+    // TODO: add isSequential test
 }
