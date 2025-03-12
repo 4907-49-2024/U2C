6051727b8c90f12cc8f1c11d153a069ee8258161
Alexandre Marques <alex.marques@live.ca>
2025-03-12 14:43:58 -0400

    Added ParallelBehavior (with test)

--- src/test/java/pipes/c2ka/primitives/BehaviorTest.java
+++ src/test/java/pipes/c2ka/primitives/BehaviorTest.java
@@ -71,4 +71,55 @@ public class BehaviorTest {
             assert behavior.toString().contains(string);
         }
     }
+
+
+    @Test
+    public void testParallelBehavior() {
+        ParallelBehavior behavior = new ParallelBehavior();
+        // Due to set collection, the order of the string may be non-deterministic. Cannot check equality always
+        Set<String> containedStrings = new HashSet<>();
+
+        // Empty
+        containedStrings.add(")");
+        containedStrings.add("(");
+        assert behavior.toString().equals("( )");
+        for (String string : containedStrings) {
+            assert behavior.toString().contains(string);
+        }
+
+        // One
+        behavior.addBehavior(new AtomicBehavior("A", "_"));
+        containedStrings.add("A");
+        assert behavior.toString().equals("( A )");
+        for (String string : containedStrings) {
+            assert behavior.toString().contains(string);
+        }
+
+        // Two
+        behavior.addBehavior(new AtomicBehavior("B", "_"));
+        containedStrings.add("||");
+        containedStrings.add("B");
+        for (String string : containedStrings) {
+            assert behavior.toString().contains(string);
+        }
+
+        // Many
+        behavior.addBehavior(new AtomicBehavior("C", "_"));
+        behavior.addBehavior(new AtomicBehavior("D", "_"));
+        containedStrings.add("C");
+        containedStrings.add("D");
+        for (String string : containedStrings) {
+            assert behavior.toString().contains(string);
+        }
+
+        // Nested Behavior
+        ChoiceBehavior behaviorNested = new ChoiceBehavior();
+        behaviorNested.addBehavior(new AtomicBehavior("F", "_"));
+        behavior.addBehavior(behaviorNested);
+        containedStrings.add("( F )");
+        assert behaviorNested.toString().equals("( F )");
+        for (String string : containedStrings) {
+            assert behavior.toString().contains(string);
+        }
+    }
 }
