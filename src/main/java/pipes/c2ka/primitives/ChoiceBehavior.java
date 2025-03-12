2f97574948a5e586063eae252beba42bb56a4e46
Alexandre Marques <alex.marques@live.ca>
2025-03-12 14:43:45 -0400

    Raised ChoiceBehavior toString to superclass

--- src/main/java/pipes/c2ka/primitives/ChoiceBehavior.java
+++ src/main/java/pipes/c2ka/primitives/ChoiceBehavior.java
@@ -10,26 +10,6 @@ public class ChoiceBehavior extends CompositeBehavior {
         super(new HashSet<>()); // Choice order does not matter
     }
 
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-
-        // Every composite encapsulated in ()
-        sb.append("(");
-        sb.append(" ");
-        for(Behavior behavior : behaviors) {
-            sb.append(behavior.toString()); // Recursively build inner behaviors
-            sb.append(" ");
-            sb.append(getCompositeOperator());
-            sb.append(" ");
-        }
-        int opIndex = sb.lastIndexOf(getCompositeOperator());
-        sb.setLength(opIndex > 0 ? opIndex : sb.length()); // Remove last operator occurrence
-        sb.append(")");
-
-        return sb.toString();
-    }
-
     /**
      * @return The operator tied to this composite, as a String
      */
--- src/main/java/pipes/c2ka/primitives/CompositeBehavior.java
+++ src/main/java/pipes/c2ka/primitives/CompositeBehavior.java
@@ -16,6 +16,26 @@ public abstract class CompositeBehavior implements Behavior {
         behaviors.add(behavior);
     }
 
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        // Every composite encapsulated in ()
+        sb.append("(");
+        sb.append(" ");
+        for(Behavior behavior : behaviors) {
+            sb.append(behavior.toString()); // Recursively build inner behaviors
+            sb.append(" ");
+            sb.append(getCompositeOperator());
+            sb.append(" ");
+        }
+        int opIndex = sb.lastIndexOf(getCompositeOperator());
+        sb.setLength(opIndex > 0 ? opIndex : sb.length()); // Remove last operator occurrence
+        sb.append(")");
+
+        return sb.toString();
+    }
+
     /**
      * @return The operator tied to this composite, as a String
      */
