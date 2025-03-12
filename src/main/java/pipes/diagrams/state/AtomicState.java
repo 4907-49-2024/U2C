a55b33f6784b042de6e1926d6018d9f113a53bef
Alexandre Marques <alex.marques@live.ca>
2025-03-12 15:41:01 -0400

    Removed parent from atomicStates
    - Renamed state -> atomic state. Only cares about its own information, no relational information.
    - Essentially a leaf in the tree

---
+++ src/main/java/pipes/diagrams/state/AtomicState.java
@@ -0,0 +1,23 @@
+package pipes.diagrams.state;
+
+/**
+ * Representation of an atomic state in a state diagram (no children, not a super state)
+ *
+ * @param name The name of the state - UNIQUE, can be used safely as a key
+ * @param kind The kind of the pseudo-state, "state" if normal state (For Papyrus, Empty = Initial)
+ * @param doActivity The doActivity, if set. Null otherwise.
+ */
+public record AtomicState(String name, String kind, String doActivity) implements State {
+    /**
+     * Converts null objects to empty strings, otherwise stores as is.
+     */
+    public AtomicState(String name, String kind, String doActivity) {
+        this.name = name == null ? "" : name;
+        this.kind = kind == null ? "" : kind;
+        this.doActivity = doActivity == null ? "" : doActivity;
+    }
+
+    public static boolean isInitialState(AtomicState state) {
+        return state != null && state.kind.isEmpty();
+    }
+}
--- src/main/java/pipes/diagrams/state/State.java
+++ src/main/java/pipes/diagrams/state/State.java
@@ -1,25 +1,7 @@
 package pipes.diagrams.state;
 
 /**
- * Representation of a state in a state diagram
- *
- * @param name The name of the state - UNIQUE, can be used safely as a key
- * @param kind The kind of the pseudo-state, "state" if normal state (For Papyrus, Empty = Initial)
- * @param doActivity The doActivity, if set. Null otherwise.
- * @param parent The superstate containing this state, if applicable. Note: if set, name should be null
+ * Interface representing state objects in a state diagram.
  */
-public record State(String name, String kind, String doActivity, State parent) implements StateDiagramElement {
-    /**
-     * Converts null objects to empty strings, otherwise stores as is.
-     */
-    public State(String name, String kind, String doActivity, State parent) {
-        this.name = name == null ? "" : name;
-        this.kind = kind == null ? "" : kind;
-        this.doActivity = doActivity == null ? "" : doActivity;
-        this.parent = parent;
-    }
-
-    public static boolean isInitialState(State state) {
-        return state != null && state.kind.isEmpty();
-    }
+public interface State extends StateDiagramElement {
 }
