692134f0babbfcd39db1d42203e3f2099471729c
Alexandre Marques <alex.marques@live.ca>
2025-03-13 17:42:49 -0400

    Add a way to get all state diagram elements in UMLModel

--- src/main/java/pipes/UMLModel.java
+++ src/main/java/pipes/UMLModel.java
@@ -3,6 +3,7 @@ package pipes;
 import com.sdmetrics.model.MetaModelElement;
 import com.sdmetrics.model.Model;
 import com.sdmetrics.model.ModelElement;
+import pipes.diagrams.state.StateType;
 
 import java.util.List;
 
@@ -16,18 +17,27 @@ public class UMLModel {
         this.model = model;
     }
 
-
     /**
      * Get elements from the model matching the given type.
      * Pre-reduces "invalid" elements this includes:
      *  - Elements with no names.
      *
      * @param typeName The type of elements to get from the model.
+     * @return The list of elements matching the given type in the model
      */
     public List<ModelElement> getTypedElements(String typeName){
         // Store given element type for parsing
         MetaModelElement type = model.getMetaModel().getType(typeName);
 
         return model.getAcceptedElements(type);
+    }
+
+    /**
+     * Get all state machine diagrams in the model.
+     *
+     * @return The list of state machine elements
+     */
+    public List<ModelElement> getStateDiagrams(){
+        return model.getAcceptedElements(model.getMetaModel().getType(StateType.statemachine.name()));
     }
 }
