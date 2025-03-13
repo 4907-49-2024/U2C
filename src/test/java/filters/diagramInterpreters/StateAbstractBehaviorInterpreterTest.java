c5bc95581615a43ad9c99bdea4ebd95754cb7013
Alexandre Marques <alex.marques@live.ca>
2025-03-13 18:02:47 -0400

    Fixing StateAbstractBehaviorInterpreterTest pipeline

--- src/test/java/filters/diagramInterpreters/StateAbstractBehaviorInterpreterTest.java
+++ src/test/java/filters/diagramInterpreters/StateAbstractBehaviorInterpreterTest.java
@@ -3,10 +3,11 @@ package filters.diagramInterpreters;
 import com.sdmetrics.model.ModelElement;
 import filters.diagramLinkers.StateDiagramLinker;
 import filters.xmiParser.XMIParser;
-import filters.xmiParser.XMIParserConfig;
+import pipes.XMIParserConfig;
 import org.junit.jupiter.api.Test;
 import pipes.UMLModel;
 import pipes.c2ka.primitives.Behavior;
+import pipes.c2ka.primitives.CompositeBehavior;
 import pipes.diagrams.state.StateDiagram;
 import pipes.diagrams.state.StateType;
 
@@ -23,31 +24,22 @@ public class StateAbstractBehaviorInterpreterTest {
      * @return Behavior type pipe object (filter under test's output)
      * @throws Exception In case of thread or input exceptions
      */
-    private Behavior runTestPipeline(String inputDiagramXMI) throws Exception {
+    private CompositeBehavior runTestPipeline(String inputDiagramXMI) throws Exception {
         // Setup Input
         String metaModel = "custom/stateMetaModel.xml";
         String xmiTrans = "custom/xmiStateTrans.xml";
+        XMIParserConfig config = new XMIParserConfig(inputDiagramXMI, xmiTrans, metaModel);
         // Filter 1
-        XMIParser parser = new XMIParser(new XMIParserConfig(inputDiagramXMI, xmiTrans, metaModel));
-        UMLModel model = parser.getModel();
-        // Assume single state diagram, get first one
-        List<ModelElement> stateDiagramsElems = model.getTypedElements(StateType.statemachine.name());
+        XMIParser parser = new XMIParser(config);
+        UMLModel model = parser.getOutput();
         // Filter 2
-        StateDiagramLinker linker = new StateDiagramLinker(stateDiagramsElems.getFirst());
-
-        // Start Thread (run filter)
-        Thread t = new Thread(linker);
-        t.start();
-        t.join();
-        StateDiagram d = linker.getStateDiagram();
-
-        // Filter 3 - Filter under test!
-        StateAbstractBehaviorInterpreter interpreter = new StateAbstractBehaviorInterpreter(d);        // Start Thread (run filter)
-        Thread filter3 = new Thread(interpreter);
-        filter3.start();
-        filter3.join();
-
-        return interpreter.getTopBehavior();
+        List<ModelElement> stateDiagramsElems = model.getStateDiagrams();
+        ModelElement stateDiagramElem = stateDiagramsElems.getFirst(); // Assume single state diagram for test case.
+        StateDiagramLinker linker = new StateDiagramLinker(stateDiagramElem);
+        StateDiagram stateDiagram = linker.getOutput();
+        // Filter 3 - FUT
+        StateAbstractBehaviorInterpreter interpreter = new StateAbstractBehaviorInterpreter(stateDiagram);
+        return interpreter.getOutput();
     }
 
     @Test
