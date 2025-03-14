225ed71cb11ca7b93b744911eafa51f4060eb44a
Alexandre Marques <alex.marques@live.ca>
2025-03-14 00:51:50 -0400

    Added NextStimTest

---
+++ src/test/java/filters/diagramInterpreters/StateNextStimInterpreterTest.java
@@ -0,0 +1,83 @@
+package filters.diagramInterpreters;
+
+import com.sdmetrics.model.ModelElement;
+import filters.diagramLinkers.StateDiagramLinker;
+import filters.xmiParser.XMIParser;
+import pipes.XMIParserConfig;
+import org.junit.jupiter.api.Test;
+import pipes.UMLModel;
+import pipes.c2ka.behaviors.AtomicBehavior;
+import pipes.c2ka.semirings.NextStimulusMap;
+import pipes.diagrams.state.SuperState;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Test the StateAbstractBehaviorInterpreterTest filter
+ */
+public class StateNextStimInterpreterTest {
+    /**
+     * Define test pipeline
+     *
+     * @param inputDiagramXMI Reference to input diagram file
+     * @return Behavior type pipe object (filter under test's output)
+     * @throws Exception In case of thread or input exceptions
+     */
+    private Set<NextStimulusMap> runTestPipeline(String inputDiagramXMI) throws Exception {
+        // Setup Input
+        String metaModel = "custom/stateMetaModel.xml";
+        String xmiTrans = "custom/xmiStateTrans.xml";
+        XMIParserConfig config = new XMIParserConfig(inputDiagramXMI, xmiTrans, metaModel);
+        // Filter 1
+        XMIParser parser = new XMIParser(config);
+        UMLModel model = parser.getOutput();
+        // Filter 2
+        List<ModelElement> stateDiagramsElems = model.getStateDiagrams();
+        ModelElement stateDiagramElem = stateDiagramsElems.getFirst(); // Assume single state diagram for test case.
+        StateDiagramLinker linker = new StateDiagramLinker(stateDiagramElem);
+        SuperState stateDiagram = linker.getOutput();
+        // Filter 3 - FUT
+        StateNextStimInterpreter interpreter = new StateNextStimInterpreter(stateDiagram);
+        return interpreter.getOutput();
+    }
+
+    // Atomic should have no transitions, so no next stims
+    @Test
+    void testAtomic() throws Exception {
+        // Get output
+        Set<NextStimulusMap> mappings = runTestPipeline("C2KA-BaseRepresentations/Atomic.uml");
+
+        for (NextStimulusMap map: mappings) {
+            // Need to do check a switch every time because of the indeterministic nature of sets.
+            switch (map.toString()){
+                default -> {assert false;} // Should not have any extra cases
+            }
+        }
+    }
+
+    // Next stim mapping has an explicit example
+    @Test
+    void testNextMappings() throws Exception {
+        // Get output
+        Set<NextStimulusMap> mappings = runTestPipeline("C2KA-BaseRepresentations/NextMappings.uml");
+        Set<NextStimulusMap> expected = new HashSet<>();
+        expected.add(new NextStimulusMap(new AtomicBehavior("Current", "<behavior-expression>"), "inStim", "nextStim"));
+        assert expected.equals(mappings);
+    }
+
+    // Sequential also has a special transition example
+    // FIXME: Ignored sequential mappings for now, still unsure how they work
+//    @Test
+//    void testSequential() throws Exception {
+//        // Get output
+//        Set<NextStimulusMap> mappings = runTestPipeline("C2KA-BaseRepresentations/Sequential.uml");
+//        Set<NextStimulusMap> expected = new HashSet<>();
+        // We're missing info, will need to update diagram at some point. I think I understand sequential better now
+        // Nevermind I'm still lost somewhat. That's okay though.
+//        expected.add(new NextStimulusMap(new AtomicBehavior("a", "<behavior-expression>"), "stimulus (a out, b in)", "stimulus (a out, b in)"));
+
+//        assert expected.equals(mappings);
+//    }
+}
