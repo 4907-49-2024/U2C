3263309507f383e086661256319d708145cdc754
Alexandre Marques <alex.marques@live.ca>
2025-03-14 13:31:31 -0400

    Created formatting functions in sink.
    - Also added two separate ways to get output (file based, and simple string output for tests)

--- src/main/java/sinks/C2KASpecifications.java
+++ src/main/java/sinks/C2KASpecifications.java
@@ -5,6 +5,8 @@ import pipes.c2ka.behaviors.CompositeBehavior;
 import pipes.c2ka.semirings.NextBehaviorMap;
 import pipes.c2ka.semirings.NextStimulusMap;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Set;
 
 /**
@@ -12,21 +14,119 @@ import java.util.Set;
  * Collects data from a few different pipes and provides a method to output this data to a file.
  *
  * @param agentName The name of the agent the specs are written for
- * @param AbstractBehaviorSpec The abstract behavior specification of the agent
+ * @param abstractBehaviorSpec The abstract behavior specification of the agent
  * @param nextBehaviorSpec The next behavior specification of the agent
  * @param nextStimulusSpec The next stimulus specification of the agent
- * @param ConcreteBehaviorSpec The concrete behavior specification of the agent
+ * @param concreteBehaviorSpec The concrete behavior specification of the agent
  */
 public record C2KASpecifications(String agentName,
-                             CompositeBehavior AbstractBehaviorSpec,
+                             CompositeBehavior abstractBehaviorSpec,
                              Set<NextBehaviorMap> nextBehaviorSpec, Set<NextStimulusMap> nextStimulusSpec,
-                             Set<AtomicBehavior> ConcreteBehaviorSpec) {
+                             Set<AtomicBehavior> concreteBehaviorSpec) {
+    private static final String OUTPUT_DIR = "../../../../Output/";
+    private static final String FILETYPE_SUFFIX = ".txt";
+
+    /**
+     * @return The filename for this C2KA specification
+     */
+    private String getFilename(){
+        return OUTPUT_DIR + agentName + FILETYPE_SUFFIX;
+    }
+
+    /**
+     * @return The abstract specification formatted in string form
+     */
+    private String getFormattedAbstractSpec(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("begin AGENT where");
+        sb.append("\n\n\t");
+
+        // Remove containing parentheses (at index 0, and index length)
+        String rightSide = abstractBehaviorSpec.toString().substring(1, abstractBehaviorSpec.toString().length() - 1);
+        sb.append(agentName);
+        sb.append(" := ");
+        sb.append(rightSide);
+
+        sb.append("\n\n");
+        sb.append("end");
+
+        return sb.toString();
+    }
+
+    /**
+     * @return The abstract specification formatted in string form
+     */
+    private String getFormattedConcreteSpec(){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("begin ");
+        sb.append("CONCRETE_BEHAVIOR"); // Type erasure in java makes this tricky to generalize without passing info
+        sb.append("  where");
+        sb.append("\n\n\t");
+
+        // Note: Order is completely random, and no whitespace to separate sections. Hopefully not a problem?
+        for(AtomicBehavior specElement: concreteBehaviorSpec){
+            sb.append(specElement.getConcreteBehavior());
+            sb.append("\n");
+        }
+
+        sb.append("\n");
+        sb.append("end");
+
+        return sb.toString();
+    }
+
+    /**
+     * @return The abstract specification formatted in string form
+     */
+    private static <T> String getFormattedMapSpec(Set<T> setSpec, String specName){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("begin ");
+        sb.append(specName); // Type erasure in java makes this tricky to generalize without passing info
+        sb.append("  where");
+        sb.append("\n\n\t");
+
+        // Note: Order is completely random, and no whitespace to separate sections. Hopefully not a problem?
+        for(T specElement: setSpec){
+            sb.append(specElement.toString());
+            sb.append("\n");
+        }
+
+        sb.append("\n");
+        sb.append("end");
+
+        return sb.toString();
+    }
+
+
+    /**
+     * @return The full specification string of this C2KASpecification sink
+     */
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(getFormattedAbstractSpec());
+        sb.append("\n\n\n");
+        sb.append(getFormattedMapSpec(nextBehaviorSpec, "NEXT_BEHAVIOR"));
+        sb.append("\n\n\n");
+        sb.append(getFormattedMapSpec(nextStimulusSpec, "NEXT_STIMULUS"));
+        sb.append("\n\n\n");
+        sb.append(getFormattedConcreteSpec());
+
+        return sb.toString();
+    }
 
     /**
      * Output formal specifications to an output file.
      * Will output it to Output/{agentName}.txt
      */
-    public void outputToFile(){
-        // TODO: build a string incrementally then dump it into the file
+    public void outputToFile() {
+        try (PrintWriter out = new PrintWriter(getFilename())) {
+            out.println(this);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
