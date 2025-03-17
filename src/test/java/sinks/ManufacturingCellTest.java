665ff8929449f53aa20f355fe89aba5a881bece1
Alexandre Marques <alex.marques@live.ca>
2025-03-16 20:45:42 -0400

    Fixing file path in test

--- src/test/java/sinks/ManufacturingCellTest.java
+++ src/test/java/sinks/ManufacturingCellTest.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 public class ManufacturingCellTest {
     private static final Path projectRoot = Paths.get(System.getProperty("user.dir"));
-    private static final Path TEST_DIR = projectRoot.resolve("test/java/TestInputs/KnownC2KASystems/ManufacturingCell");
+    private static final Path TEST_DIR = projectRoot.resolve("src/test/java/TestInputs/KnownC2KASystems/ManufacturingCell");
 
     /**
      * Define test pipeline
@@ -31,7 +31,7 @@ public class ManufacturingCellTest {
         // Setup Input
         String metaModel = "custom/stateMetaModel.xml";
         String xmiTrans = "custom/xmiStateTrans.xml";
-        XMIParserConfig config = new XMIParserConfig(inputDiagramXMI, xmiTrans, metaModel);
+        XMIParserConfig config = new XMIParserConfig(TEST_DIR, inputDiagramXMI, xmiTrans, metaModel);
         // Filter 1
         XMIParser parser = new XMIParser(config);
         UMLModel model = parser.getOutput();
@@ -51,71 +51,8 @@ public class ManufacturingCellTest {
     }
 
     @Test
-    void testAtomic() throws Exception {
-        // Get output
-        C2KASpecifications specs = runTestPipeline("Atomic.uml");
-        String expectedFormat = "begin AGENT where\n" +
-                "\n" +
-                "\tAtomic Behavior :=  <name> \n" +
-                "\n" +
-                "end\n" +
-                "\n" +
-                "\n" +
-                "begin NEXT_BEHAVIOR  where\n" +
-                "\n" +
-                "\t\n" +
-                "end\n" +
-                "\n" +
-                "\n" +
-                "begin NEXT_STIMULUS  where\n" +
-                "\n" +
-                "\t\n" +
-                "end\n" +
-                "\n" +
-                "\n" +
-                "begin CONCRETE_BEHAVIOR  where\n" +
-                "\n" +
-                "\t<name> => [ <behavior-expression> ]\n" +
-                "\n" +
-                "end";
-
-        assert specs.toString().equals(expectedFormat);
+    void testControlAgent() throws Exception {
+        C2KASpecifications specifications = runTestPipeline("Control Agent.uml");
+        specifications.outputToFile(); // TODO: Augment this test with a diff tool
     }
-
-    @Test
-    void test() throws Exception {
-        // Get output
-        C2KASpecifications specs = runTestPipeline("Atomic.uml");
-        String expectedFormat = "begin AGENT where\n" +
-                "\n" +
-                "\tAtomic Behavior :=  <name> \n" +
-                "\n" +
-                "end\n" +
-                "\n" +
-                "\n" +
-                "begin NEXT_BEHAVIOR  where\n" +
-                "\n" +
-                "\t\n" +
-                "end\n" +
-                "\n" +
-                "\n" +
-                "begin NEXT_STIMULUS  where\n" +
-                "\n" +
-                "\t\n" +
-                "end\n" +
-                "\n" +
-                "\n" +
-                "begin CONCRETE_BEHAVIOR  where\n" +
-                "\n" +
-                "\t<name> => [ <behavior-expression> ]\n" +
-                "\n" +
-                "end";
-
-        assert specs.toString().equals(expectedFormat);
-    }
-
-
-    // TODO: implement system tests here.
-    //  Maybe the other C2KA_BaseRepresentations?
-    //  Mostly interested in the real sample outputs
 }
