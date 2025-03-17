1512644f4b2ae3e4b39aac3577929817262adef3
Alexandre Marques <alex.marques@live.ca>
2025-03-16 20:12:00 -0400

    Making sink test use new test directory

--- src/test/java/sinks/C2KASpecificationsTest.java
+++ src/test/java/sinks/C2KASpecificationsTest.java
@@ -12,9 +12,13 @@ import pipes.UMLModel;
 import pipes.XMIParserConfig;
 import pipes.diagrams.state.SuperState;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class C2KASpecificationsTest {
+    private static final Path projectRoot = Paths.get(System.getProperty("user.dir"));
+    private static final Path TEST_DIR = projectRoot.resolve("src/test/java/TestInputs/C2KA-BaseRepresentations");
     /**
      * Define test pipeline
      *
@@ -26,7 +30,7 @@ public class C2KASpecificationsTest {
         // Setup Input
         String metaModel = "custom/stateMetaModel.xml";
         String xmiTrans = "custom/xmiStateTrans.xml";
-        XMIParserConfig config = new XMIParserConfig(inputDiagramXMI, xmiTrans, metaModel);
+        XMIParserConfig config = new XMIParserConfig(TEST_DIR, inputDiagramXMI, xmiTrans, metaModel);
         // Filter 1
         XMIParser parser = new XMIParser(config);
         UMLModel model = parser.getOutput();
@@ -48,7 +52,7 @@ public class C2KASpecificationsTest {
     @Test
     void testAtomic() throws Exception {
         // Get output
-        C2KASpecifications specs = runTestPipeline("C2KA-BaseRepresentations/Atomic.uml");
+        C2KASpecifications specs = runTestPipeline("Atomic.uml");
         String expectedFormat = "begin AGENT where\n" +
                 "\n" +
                 "\tAtomic Behavior :=  <name> \n" +
@@ -76,8 +80,4 @@ public class C2KASpecificationsTest {
 
         assert specs.toString().equals(expectedFormat);
     }
-
-    // TODO: implement system tests here.
-    //  Maybe the other C2KA_BaseRepresentations?
-    //  Mostly interested in the real sample outputs
 }
