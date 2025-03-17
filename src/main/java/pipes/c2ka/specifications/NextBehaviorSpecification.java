470ef259a32915468a58d6a743180fe86e508822
Alexandre Marques <alex.marques@live.ca>
2025-03-17 11:54:03 -0400

    Implemented Specification classes for next mappings

---
+++ src/main/java/pipes/c2ka/specifications/NextBehaviorSpecification.java
@@ -0,0 +1,16 @@
+package pipes.c2ka.specifications;
+
+import pipes.c2ka.semirings.NextBehaviorMap;
+
+import java.util.Set;
+
+public class NextBehaviorSpecification extends NextMappingSpecification<NextBehaviorMap> {
+    public NextBehaviorSpecification(Set<NextBehaviorMap> mappings) {
+        super(mappings);
+    }
+
+    @Override
+    public String getSpecificationName() {
+        return "NEXT_BEHAVIOUR";
+    }
+}
---
+++ src/main/java/pipes/c2ka/specifications/NextMappingSpecification.java
@@ -0,0 +1,39 @@
+package pipes.c2ka.specifications;
+
+import pipes.c2ka.semirings.NextMapSemiring;
+
+import java.util.Set;
+
+/**
+ * Specifications parent class for NextMappings. Takes in sets of NextMap elements to make a specification out of them.
+ * @param <mapType> The NextMapping type, nextBehavior or nextStimulus
+ */
+public abstract class NextMappingSpecification<mapType extends NextMapSemiring<?>> implements Specification {
+    private final Set<mapType> mappings;
+
+    protected NextMappingSpecification(Set<mapType> mappings) {
+        this.mappings = mappings;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("begin ");
+        sb.append(getSpecificationName()); // Type erasure in java makes this tricky to generalize without passing info
+        sb.append("  where");
+        sb.append("\n\n\t");
+
+        // Note: Order is completely random, and no whitespace to separate sections. Hopefully not a problem?
+        for(mapType mapping: mappings.stream().sorted().toList()){
+            sb.append(mapping.toString());
+            sb.append("\n\t");
+        }
+
+        sb.append("\n");
+        sb.append("end");
+
+        return sb.toString();
+    }
+}
