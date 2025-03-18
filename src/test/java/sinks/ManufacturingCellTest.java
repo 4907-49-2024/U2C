1c789709f1b073ce04dc2aeafa35af77200d97b3
Alexandre Marques <alex.marques@live.ca>
2025-03-17 23:29:03 -0400

    Fixing stimuli list carrying over on other tests

--- src/test/java/sinks/ManufacturingCellTest.java
+++ src/test/java/sinks/ManufacturingCellTest.java
@@ -1,7 +1,9 @@
 package sinks;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pipelines.Main;
+import pipes.c2ka.Stimulus;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -12,6 +14,11 @@ import static sinks.SpecificationDiffTool.specificationDiff;
 public class ManufacturingCellTest {
     private static final Path projectRoot = Paths.get(System.getProperty("user.dir"));
     private static final Path TEST_DIR = projectRoot.resolve("src/test/java/TestInputs/KnownC2KASystems/ManufacturingCell");
+
+    @BeforeEach
+    void clearStimuli(){
+        Stimulus.getSystemStimuli().clear();
+    }
 
     @Test
     void testManufacturingCell() throws Exception {
