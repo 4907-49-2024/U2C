de50406f822abb3a5deee2d5c57a432f7083b0e7
Alexandre Marques <alex.marques@live.ca>
2025-03-18 17:25:26 -0400

    Changing how we identify sequential transitions to avoid collisions

--- src/main/java/filters/diagramInterpreters/utils/StateBehaviorConverter.java
+++ src/main/java/filters/diagramInterpreters/utils/StateBehaviorConverter.java
@@ -24,14 +24,14 @@ public class StateBehaviorConverter {
     private static CompositeBehavior instantiateComposite(SuperState superState) throws IllegalStateException {
         boolean isParallel  = superState.numRegions() > 1;
         // If any transitions are sequential, should be a sequential state
-        boolean isSequential  = superState.innerTransitions().stream().anyMatch(Transition::isSequentialTransition);
+        boolean isSequential  = superState.innerTransitions().stream().anyMatch(Transition::isSequential);
 
         // Error checks (model validation)
         if (isParallel && isSequential) {
             throw new IllegalStateException("Simultaneously parallel and sequential composite behaviors are not supported");
         }
 
-        boolean isEntirelySequential = superState.innerTransitions().stream().allMatch(Transition::isSequentialTransition);
+        boolean isEntirelySequential = superState.innerTransitions().stream().allMatch(Transition::isSequential);
         if (isSequential && !isEntirelySequential) {
             throw new IllegalStateException("Partially sequential composite behaviors are not supported");
         }
