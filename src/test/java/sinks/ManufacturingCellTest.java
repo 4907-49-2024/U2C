ee504c05d655aa5b75be443c4feb50fc602ceebf
Alexandre Marques <alex.marques@live.ca>
2025-03-17 23:20:15 -0400

    Updating test input locations

--- src/test/java/sinks/ManufacturingCellTest.java
+++ src/test/java/sinks/ManufacturingCellTest.java
@@ -3,18 +3,23 @@ package sinks;
 import org.junit.jupiter.api.Test;
 import pipelines.Main;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 import static sinks.SpecificationDiffTool.specificationDiff;
 
 public class ManufacturingCellTest {
+    private static final Path projectRoot = Paths.get(System.getProperty("user.dir"));
+    private static final Path TEST_DIR = projectRoot.resolve("src/test/java/TestInputs/KnownC2KASystems/ManufacturingCell");
+
     @Test
     void testManufacturingCell() throws Exception {
         List<String> inputs = new ArrayList<>();
-        inputs.add("Control Agent.uml");
-        inputs.add("Handling Agent.uml");
-        inputs.add("Processing Agent.uml");
-        inputs.add("Storage Agent.uml");
+        inputs.add(TEST_DIR+"\\Control Agent.uml");
+        inputs.add(TEST_DIR+"\\Handling Agent.uml");
+        inputs.add(TEST_DIR+"\\Processing Agent.uml");
+        inputs.add(TEST_DIR+"\\Storage Agent.uml");
         // Parse system - Generalize main later?
         Set<C2KASpecifications> specs = Main.runMainPipeline(inputs);
 
