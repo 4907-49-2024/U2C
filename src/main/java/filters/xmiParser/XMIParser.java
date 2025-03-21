0dfe111b571b5d9fb945fa3fa3d1be52cdb8e5f4
Alexandre Marques <alex.marques@live.ca>
2025-03-21 17:57:15 -0400

    Making XMIParser return ModelElement

--- src/main/java/filters/xmiParser/XMIParser.java
+++ src/main/java/filters/xmiParser/XMIParser.java
@@ -4,14 +4,27 @@ import com.sdmetrics.model.*;
 import com.sdmetrics.util.XMLParser;
 import filters.Filter;
 import org.xml.sax.SAXException;
-import pipes.UMLModel;
 import pipes.XMIParserConfig;
+import pipes.diagrams.state.StateType;
 
 import javax.xml.parsers.ParserConfigurationException;
+import java.util.List;
 
-public class XMIParser extends Filter<XMIParserConfig, UMLModel> {
+public class XMIParser extends Filter<XMIParserConfig, ModelElement> {
     public XMIParser(XMIParserConfig config) {
         super(config);
+    }
+
+    private ModelElement getDiagram(Model model){
+        // TODO: Revisit this line when handling multiple diagram types
+        // Assumption only handle state machines
+        String diagramType = StateType.statemachine.name();
+
+        MetaModelElement type = model.getMetaModel().getType(diagramType);
+        List<ModelElement> diagramElems = model.getAcceptedElements(type);
+
+        // Assumption - only one diagram per file (should hold, according to our input specification)
+        return  diagramElems.getFirst();
     }
 
     /**
@@ -50,6 +63,6 @@ public class XMIParser extends Filter<XMIParserConfig, UMLModel> {
             throw new RuntimeException(e);
         }
 
-        output = new UMLModel(model);
+        output = getDiagram(model);
     }
 }
