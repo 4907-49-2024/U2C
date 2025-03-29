fca4105eb7dfe0a2c1d3b2e22a350553ea7e97b0
Alexandre Marques <alex.marques@live.ca>
2025-03-29 17:57:09 -0400

    Added ConcreteBehaviorSpecification

---
+++ src/main/java/pipes/c2ka/specifications/ConcreteBehaviorSpecification.java
@@ -0,0 +1,40 @@
+package pipes.c2ka.specifications;
+
+import pipes.c2ka.behaviors.AtomicBehavior;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConcreteBehaviorSpecification extends Specification {
+    private final Set<AtomicBehavior> atomicBehaviors;
+
+    public ConcreteBehaviorSpecification() {
+        this.atomicBehaviors = new HashSet<>();
+    }
+
+    /**
+     * Add an atomic behavior to this concrete behavior specification
+     * @param atomicBehavior The atomic behavior to add to the spec.
+     */
+    public void add(AtomicBehavior atomicBehavior) {
+        atomicBehaviors.add(atomicBehavior);
+    }
+
+    @Override
+    public String getSpecificationContents() {
+        StringBuilder sb = new StringBuilder();
+
+        // Note: Order is alphabetically sorted, and no whitespace to separate sections.
+        for(AtomicBehavior specElement: atomicBehaviors.stream().sorted().toList()){
+            sb.append(specElement.getConcreteBehavior());
+            sb.append("\n\t");
+        }
+
+        return sb.toString();
+    }
+
+    @Override
+    public String getSpecificationName() {
+        return "CONCRETE_BEHAVIOUR";
+    }
+}
