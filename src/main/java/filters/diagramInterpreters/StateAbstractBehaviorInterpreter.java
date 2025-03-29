325049f1d84e97346eb9041e82a412331d33e4bb
Alexandre Marques <alex.marques@live.ca>
2025-03-29 18:11:52 -0400

    Updated interfaces for filters

--- src/main/java/filters/diagramInterpreters/StateAbstractBehaviorInterpreter.java
+++ src/main/java/filters/diagramInterpreters/StateAbstractBehaviorInterpreter.java
@@ -3,6 +3,7 @@ package filters.diagramInterpreters;
 import filters.Filter;
 import filters.diagramInterpreters.utils.StateBehaviorConverter;
 import pipes.c2ka.behaviors.*;
+import pipes.c2ka.specifications.AbstractBehaviorSpecification;
 import pipes.diagrams.state.*;
 
 import java.util.Set;
@@ -12,7 +13,7 @@ import java.util.Set;
  * The StateAbstractBehaviorInterpreter takes in a StateDiagram, and returns a representation of its AbstractBehaviors.
  * The output's toString returns the AbstractBehavior of the entire agent.
  */
-public class StateAbstractBehaviorInterpreter extends Filter<SuperState, CompositeBehavior> {
+public class StateAbstractBehaviorInterpreter extends Filter<SuperState, AbstractBehaviorSpecification> {
     public StateAbstractBehaviorInterpreter(SuperState diagram) {
         super(diagram);
     }
@@ -23,6 +24,7 @@ public class StateAbstractBehaviorInterpreter extends Filter<SuperState, Composi
     @Override
     public void run() {
         // Can assume the input given is a composite because the input is a SuperState
-        output = (CompositeBehavior) StateBehaviorConverter.getStateBehavior(input);
+        CompositeBehavior topLevelBehavior = (CompositeBehavior) StateBehaviorConverter.getStateBehavior(input);
+        output = new AbstractBehaviorSpecification(input.name(), topLevelBehavior);
     }
 }
