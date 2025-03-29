1a16d3de3831090aa304d77b83b55077ceb57b37
Alexandre Marques <alex.marques@live.ca>
2025-03-29 17:59:11 -0400

    Added AbstractBehaviorSpecification

---
+++ src/main/java/pipes/c2ka/specifications/AbstractBehaviorSpecification.java
@@ -0,0 +1,32 @@
+package pipes.c2ka.specifications;
+
+import pipes.c2ka.behaviors.CompositeBehavior;
+
+
+public class AbstractBehaviorSpecification extends Specification {
+    private final CompositeBehavior topLevelBehavior;
+    private final String agentName;
+
+    public AbstractBehaviorSpecification(String agentName, CompositeBehavior topLevelBehavior) {
+        this.topLevelBehavior = topLevelBehavior;
+        this.agentName = agentName;
+    }
+
+    @Override
+    public String getSpecificationContents() {
+        StringBuilder sb = new StringBuilder();
+
+        // Remove containing parentheses (at index 0, and index length)
+        String rightSide = topLevelBehavior.toString().substring(1, topLevelBehavior.toString().length() - 1);
+        sb.append(agentName);
+        sb.append(" := ");
+        sb.append(rightSide);
+
+        return sb.toString();
+    }
+
+    @Override
+    public String getSpecificationName() {
+        return "AGENT";
+    }
+}
