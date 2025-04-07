5d3cad3412bc4c3e31540d8be2d9cb72c9c04021
Michael Rochefort <145927893+MikeHabs1999@users.noreply.github.com>
2025-04-07 17:06:43 -0400

    Update XMIParserConfigTest.java

--- src/test/java/pipes/XMIParserConfigTest.java
+++ src/test/java/pipes/XMIParserConfigTest.java
@@ -1,48 +1,42 @@
 package pipes;
 
 import org.junit.jupiter.api.Test;
+import pipes.parserConfig.XMIParserConfig;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class XMIParserConfigTest {
-    // TODO: Fix test, there is no such thing as valid defaults anymore. Be explicit in testing
-//    // Want to test that the default implementation is equivalent to passing in the default file
-//    private static final XMIParserConfig defaultConfig = new XMIParserConfig();
-//    private static final XMIParserConfig inputConfig = new XMIParserConfig(XMIParserConfig.DEFAULT_INPUT);
-//    private static final XMIParserConfig fullConfig = new XMIParserConfig(XMIParserConfig.DEFAULT_INPUT, XMIParserConfig.XMI_TRANSFO_NAME, XMIParserConfig.META_MODEL_NAME);
-//
-//    @Test
-//    void testMetaModel() {
-//        // Assert equivalence for default behavior
-//        assert Objects.equals(defaultConfig.metaModel(), inputConfig.metaModel());
-//        assert Objects.equals(defaultConfig.metaModel(), fullConfig.metaModel());
-//
-//        // Assert equivalence to expected name
-//        Path metaModelPath = Paths.get(defaultConfig.metaModel());
-//        assert metaModelPath.getFileName().toString().equals(XMIParserConfig.META_MODEL_NAME);
-//    }
-//
-//    @Test
-//    void testXmiTrans() {
-//        // Assert equivalence for default behavior
-//        assert Objects.equals(defaultConfig.xmiTrans(), inputConfig.xmiTrans());
-//        assert Objects.equals(defaultConfig.xmiTrans(), fullConfig.xmiTrans());
-//
-//        // Assert equivalence to expected name
-//        Path path = Paths.get(defaultConfig.xmiTrans());
-//        assert path.getFileName().toString().equals(XMIParserConfig.XMI_TRANSFO_NAME);
-//    }
-//
-//    @Test
-//    void testXmiFileName() {
-//        // Assert equivalence for default behavior
-//        assert Objects.equals(defaultConfig.xmiInputFile(), inputConfig.xmiInputFile());
-//        assert Objects.equals(defaultConfig.xmiInputFile(), fullConfig.xmiInputFile());
-//
-//        // Assert equivalence to expected name
-//        Path path = Paths.get(defaultConfig.xmiInputFile());
-//        assert path.getFileName().toString().equals(XMIParserConfig.DEFAULT_INPUT);
-//    }
+    private final String testInputFile = "Atomic.uml";
+    private final String testTransform = "custom/xmiStateTrans.xml";
+    private final String testMetaModel = "custom/stateMetaModel.xml";
+
+    @Test
+    void testConfigFromDefaultInputDir() {
+        XMIParserConfig config = new XMIParserConfig(testInputFile, testTransform, testMetaModel);
+
+        Path expectedInputPath = Paths.get(System.getProperty("user.dir")).resolve("Input").resolve(testInputFile);
+        Path expectedTransformPath = Paths.get(System.getProperty("user.dir")).resolve("src/main/resources/sdmetrics").resolve(testTransform);
+        Path expectedMetaModelPath = Paths.get(System.getProperty("user.dir")).resolve("src/main/resources/sdmetrics").resolve(testMetaModel);
+
+        assertEquals(expectedInputPath.toString(), config.xmiInputFile());
+        assertEquals(expectedTransformPath.toString(), config.xmiTrans());
+        assertEquals(expectedMetaModelPath.toString(), config.metaModel());
+    }
+
+    @Test
+    void testConfigFromCustomInputDir() {
+        Path customInput = Paths.get("src/test/resources/testInputs");
+        XMIParserConfig config = new XMIParserConfig(customInput, testInputFile, testTransform, testMetaModel);
+
+        Path expectedInputPath = customInput.resolve(testInputFile);
+        Path expectedTransformPath = Paths.get(System.getProperty("user.dir")).resolve("src/main/resources/sdmetrics").resolve(testTransform);
+        Path expectedMetaModelPath = Paths.get(System.getProperty("user.dir")).resolve("src/main/resources/sdmetrics").resolve(testMetaModel);
+
+        assertEquals(expectedInputPath.toString(), config.xmiInputFile());
+        assertEquals(expectedTransformPath.toString(), config.xmiTrans());
+        assertEquals(expectedMetaModelPath.toString(), config.metaModel());
+    }
 }
